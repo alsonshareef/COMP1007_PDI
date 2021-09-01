c3cea1aadb17f87a477c130ec5fb945969d2a92e
Alson Shareef <alson.shareef@gmail.com>
2021-09-01 19:27:08 +0800

    Added Assessments directory for COMP1007

---
+++ Assessments/midsem/TaskTwo.java
@@ -0,0 +1,15 @@
+/** 
+ * AUTHOR: Alson Shareef
+ * PURPOSE: Java Program for Task Two of Mid Sememester Assessment
+ * DATE: 01/09/2021
+*/
+
+import java.util.*;
+
+public class TaskTwo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This is the Java Program for Task Two of Mid Sem");
+    }
+}
