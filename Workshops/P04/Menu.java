5dcdd141c4182809b882247f73c07cb93945323e
Alson Shareef <alson.shareef@gmail.com>
2021-09-01 16:14:40 +0800

    Added divisible functionality for third menu option

--- Workshops/P04/Menu.java
+++ Workshops/P04/Menu.java
@@ -23,51 +23,64 @@ public class Menu
             System.out.println("> 0. Exit"); 
             option = input.nextInt();
 
-            // 2. Run corresponding code if user selected a valid option.
+            // 2. Run corresponding code to the valid option user provided.
             switch(option)
             {
-                // 2.a) Exit program
+                // 2.a) Exit program if option equals 0
                 case 0:
                     System.out.println("Goodbye!");
                 break;
 
-                // 2.b) Take 2 integers from user and output the sum
+                // 2.b) Take 2 integers from user and output the sum if option
+                //      equals 1
                 case 1:
                     int sumIntOne, sumIntTwo, sum;
                     System.out.println("Enter first integer");
                     sumIntOne = input.nextInt();
                     System.out.println("Enter second integer");
                     sumIntTwo = input.nextInt();
-
                     sum = sumIntOne + sumIntTwo;
-
                     System.out.println("The sum of " + sumIntOne + " and " + 
                                         sumIntTwo + " is equal to: " + sum);
-                    
                 break;
 
-                // 2.c) Take 2 integers from user and output the product
+                // 2.c) Take 2 integers from user and output the product if 
+                //      option equals 2
                 case 2:
                     int prodIntOne, prodIntTwo, product;
                     System.out.println("Enter first integer");
                     prodIntOne = input.nextInt();
                     System.out.println("Enter second integer");
                     prodIntTwo = input.nextInt();
-
                     product = prodIntOne * prodIntTwo;
                     System.out.println("The product of " + prodIntOne + " and " +
                                         prodIntTwo + " is equal to: " + product);
                 break;
 
                 // 2.d) Take two integers and output whether they are divisible
+                //      if option equals 3
                 case 3:
-                    System.out.print("Determine if 2 Integers are Divisible \n");
+                    int divIntOne, divIntTwo;
+                    System.out.println("Enter first integer");
+                    divIntOne = input.nextInt();
+                    System.out.println("Enter second integer");
+                    divIntTwo = input.nextInt();
+                    if (divIntOne % divIntTwo == 0)
+                    {
+                        System.out.println(divIntOne + " and " + divIntTwo +
+                        " are divisible");
+                    }
+                    else 
+                    {
+                        System.out.println(divIntOne + " and " + divIntTwo +
+                        " are not divisible");
+                    }
                 break;
 
-                // 2.e) If input was invalid, request user to input valid
-                //      option
+                // 2.e) Request user to input valid option
+                //      if option < 0 or option > 3
                 default:
-                    System.out.println("Option does not exist. Please select a "
+                    System.out.println("Option was invalid. Please select a "
                                                         + "valid option.");
             }        
         } while ((option < 0) || (option > 3));
