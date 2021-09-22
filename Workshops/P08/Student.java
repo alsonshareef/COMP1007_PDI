0748bb2704df6c5a70c205f38040c9b31643dad6
Alson Shareef <alson.shareef@gmail.com>
2021-09-22 14:45:54 +0800

    Workshop 8

---
+++ Workshops/P08/Student.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Student
+{
+    // Class fields
+    private String name;
+    private Integer studentId;
+    private double mark;
+    
+    // Main
+    public static void main(String [] args)
+    {
+        System.out.println("Student");        
+    }
+
+}
