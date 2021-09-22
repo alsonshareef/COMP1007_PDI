bfbfd2c95a89b833181e3bec96d5f7e2539fdd20
alsonshareef <alson.shareef@gmail.com>
2021-09-22 17:56:13 +0800

    Student class stuff

--- Workshops/P08/Student.java
+++ Workshops/P08/Student.java
@@ -1,16 +1,15 @@
 import java.util.*;
 
-public class Student
+public class Student 
 {
-    // Class fields
-    private String name;
-    private Integer studentId;
+    // CLASS FIELDS
+    private String: name;
+    private int studentId;
     private double mark;
     
-    // Main
-    public static void main(String [] args)
-    {
-        System.out.println("Student");        
-    }
+    // CONSTRUCTORS
+    // ACCESSOR METHODS
+    // MUTATOR METHODS
+    // VALIDATION METHODS
 
 }
