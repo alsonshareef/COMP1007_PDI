a9d8e749cb7f22444d11bb00db69bec1609dd2c8
Alson Shareef <alson.shareef@gmail.com>
2021-10-11 03:19:28 +0800

    updated covidcase test

--- Assessments/assignment/CovidCaseTest.java
+++ Assessments/assignment/CovidCaseTest.java
@@ -14,6 +14,37 @@ public class CovidCaseTest
         System.out.println("Default Covid Case CASES: " + defaultCovidCase.getCases());
         System.out.println("Default Covid Case DATE: " + defaultCovidCase.getDate().toString());
 
+        System.out.println();
+
+        // Mutate Default Covid Case
+        defaultCovidCase.setCountry("Australia");
+        defaultCovidCase.setProvince("Perth");
+        defaultCovidCase.setRegion("WA");
+        defaultCovidCase.setAgeGroup("89-99");
+        defaultCovidCase.setSex("F");
+        defaultCovidCase.setCases(69);
+        defaultCovidCase.setDate(4,10,3000);
+
+        System.out.println("Default Covid Case COUNTRY: " + defaultCovidCase.getCountry());
+        System.out.println("Default Covid Case PROVINCE: " + defaultCovidCase.getProvince());
+        System.out.println("Default Covid Case REGION: " + defaultCovidCase.getRegion());
+        System.out.println("Default Covid Case AGE GROUP: " + defaultCovidCase.getAgeGroup());
+        System.out.println("Default Covid Case SEX: " + defaultCovidCase.getSex());
+        System.out.println("Default Covid Case CASES: " + defaultCovidCase.getCases());
+        System.out.println("Default Covid Case DATE: " + defaultCovidCase.getDate().toString());
+
+        System.out.println();
+
+        // Covid Case Param Consuctor
+        CovidCase paramCovidCase = new CovidCase("United States", "New York", "East", "0-9", "F", 420, 5, 7, 1996);
+        System.out.println("Default Covid Case COUNTRY: " + paramCovidCase.getCountry());
+        System.out.println("Default Covid Case PROVINCE: " + paramCovidCase.getProvince());
+        System.out.println("Default Covid Case REGION: " + paramCovidCase.getRegion());
+        System.out.println("Default Covid Case AGE GROUP: " + paramCovidCase.getAgeGroup());
+        System.out.println("Default Covid Case SEX: " + paramCovidCase.getSex());
+        System.out.println("Default Covid Case CASES: " + paramCovidCase.getCases());
+        System.out.println("Default Covid Case DATE: " + paramCovidCase.getDate().toString());
+
     }
 
 }
